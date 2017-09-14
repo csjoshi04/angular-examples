6ec261b3ac6848faf94c66068b91d6e2795a914f
Chandra Shekhar Joshi <cj.github@gmail.com>
2017-09-14 23:23:28 +0530

    reactive form add form controls, submit

--- app/reactive-form/component/app.component.ts
+++ app/reactive-form/component/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 require('file?name=[name].[ext]!./app.component.tpl.html');
 require('file?name=[name].[ext]!./app.component.css');
@@ -8,6 +9,19 @@ require('file?name=[name].[ext]!./app.component.css');
   templateUrl: 'app.component.tpl.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
+  signupForm:FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username':new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit(){
+    console.log(this.signupForm)
+  }
 }
--- app/reactive-form/module/reactive-form.module.ts
+++ app/reactive-form/module/reactive-form.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { BrowserModule }  from '@angular/platform-browser';
-import { AppComponent } from '../component/app.component';
+import {NgModule} from "@angular/core";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "../component/app.component";
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent
