a307bf7dd851293f9fa05a013c67491b8760e723
Chandra Shekhar Joshi <cj.github@gmail.com>
2017-09-15 09:19:03 +0530

    reactive form custom validator, form array, grouping, validation

--- app/reactive-form/component/app.component.ts
+++ app/reactive-form/component/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 require('file?name=[name].[ext]!./app.component.tpl.html');
 require('file?name=[name].[ext]!./app.component.css');
@@ -12,16 +12,32 @@ require('file?name=[name].[ext]!./app.component.css');
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm:FormGroup;
+  forbiddenUserNames:string[] = ["Chris","Anna"];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'username':new FormControl(null),
-      'email': new FormControl(null),
+      'userData': new FormGroup({
+        'username':new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null,[Validators.required, Validators.email]),
+      }),
+      'hobbies': new FormArray([]),
       'gender': new FormControl('male')
     });
   }
 
   onSubmit(){
     console.log(this.signupForm)
+  }
+
+  onAddHobby(){
+    let control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get("hobbies")).push(control)
+  }
+
+  forbiddenNames(formControl:FormControl):{[s: string]:boolean}{
+    if(this.forbiddenUserNames.indexOf(formControl.value) > -1){
+      return {'nameIsForbidden':true};
+    }
+    return null;
   }
 }
